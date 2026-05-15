14074d1b87131f8e5c6812662acd74612822d7ef
Robin Walter <hello@robinwalter.me>
2026-05-15 19:53:25 +0200

    feat(flake): use `nix/dev/flake-module.nix` as `dev` partition
    Signed-off-by: Robin Walter <hello@robinwalter.me>

--- flake.nix
+++ flake.nix
@@ -54,7 +54,7 @@
             module =
               { ... }:
               {
-                imports = [ ];
+                imports = [ ./nix/dev/flake-module.nix ];
               };
           };
         };
