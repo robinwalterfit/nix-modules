87c4f06e5b6f9189320cc26c924e2314ad2f69bc
Robin Walter <hello@robinwalter.me>
2026-06-04 17:42:16 +0200

    fix(templates): add missing `home-manager-config` to `templates` in `flake.nix`
    Signed-off-by: Robin Walter <hello@robinwalter.me>

--- flake.nix
+++ flake.nix
@@ -34,12 +34,43 @@
 
         # Provide templates
         templates = {
+          home-manager-config = {
+            description = "A Home-Manager Configuration template based on Nix Flakes, flake-parts and using robinwalterfit/nix-home-modules";
+            path = ./templates/home-manager-config;
+
+            # REUSE-IgnoreStart
+            welcomeText = ''
+              # Home Manager Configuration Template
+
+              A Home-Manager Configuration template based on Nix Flakes, flake-parts and using robinwalterfit/nix-home-modules.
+
+              ## Prerequisites
+
+              - **Check the README for any template content and replace it with your own**
+              - **[Choose a license](https://choosealicense.com/) and update the [LICENSE file](./LICENSE)**
+              - **Search for the following and replace the content accordingly:**
+                  - `SPDX-FileCopyrightText: NONE`
+                  - `SPDX-License-Identifier: NONE`
+                  - `SPDX-FileCopyrightText = 'NONE'`
+                  - `SPDX-License-Identifier = 'NONE'`
+                  - `<GITHUB_HANDLE>`
+                  - `<HM_SYSTEM>`
+                  - `<HM_USERNAME>`
+                  - `<PROJECT_DESCRIPTION>`
+                  - `<PROJECT_NAME>`
+                  - `'NONE'`
+              - **If you want to keep the Contributor Covenant, review the [Code of Conduct](./.github/CODE_OF_CONDUCT.md) and replace any template placeholders**
+              - **In order for the flake with the `dev` partition to work, a `flake.lock` is required in `nix/dev`. Run `cd nix/dev` followed by `nix flake lock` to create the lock file**
+              - **Run `fd --hidden --type file '.gitkeep' --exec rm` or `find . -name '.gitkeep' -delete` to remove all `.gitkeep` files in the template**
+            '';
+            # REUSE-IgnoreEnd
+          };
           simple = {
             description = "A simple template to start from scratch.";
             path = ./templates/simple;
           };
 
-          defaultTemplate = self.templates.simple;
+          default = self.templates.simple;
         };
       in
       {
